cd25e5b32af57f9cd813595b177692d0420aeddb
gnalin761 <Girish_Nalinde@comcast.com>
2025-07-31 14:02:45 +0530

    Update get_row_description_udf.c

--- get_row_description_udf.c
+++ get_row_description_udf.c
@@ -1,11 +1,21 @@
-#define SQL_TEXT Latin_Text
-#include <sqltypes_td.h>
 #include <string.h>
+#include <stdio.h>
+
+// Define SQL_TEXT before including sqltypes_td.h
+typedef unsigned char Latin_Text;
+typedef Latin_Text SQL_TEXT;
+
+#include <sqltypes_td.h>
+
+// Define other SQL types
+typedef float SQL_FLOAT_T;
+typedef int SQL_INT_T;
+typedef char SQL_BOOLEAN_T;
 
 void get_row_description_udf(
     char          *result_data,
     int            result_len,
-    INTEGER       *rownum,
+    int           *rownum,
     char           sqlstate[6],
     SQL_FLOAT_T   *scratchpad,
     SQL_INT_T     *calltype,
@@ -13,6 +23,7 @@ void get_row_description_udf(
     SQL_TEXT       extname[129],
     SQL_TEXT       specific_name[129],
     SQL_TEXT       error_message[257],
+    int            error_message_len,
     SQL_TEXT       udf_name[129],
     SQL_TEXT       udf_specific_name[129],
     SQL_TEXT       udf_schema_name[129],
@@ -20,32 +31,26 @@ void get_row_description_udf(
     SQL_TEXT       udf_database_name[129]
 )
 {
+    // Set default SQLSTATE to success
     strcpy(sqlstate, "00000");
-    error_message[0] = '\0';
 
+    // Clear error message buffer
+    if (error_message_len > 0) {
+        error_message[0] = '\0';
+    }
+
+    // Check if rownum is NULL
     if (isnull[1]) {
-        strcpy(sqlstate, "22002");
-        strncpy(error_message, "Input 'rownum' cannot be NULL for this function.", sizeof(error_message) - 1);
-        error_message[sizeof(error_message) - 1] = '\0';
-        isnull[0] = 1;
+        strcpy(sqlstate, "22002"); // NULL input error
+        if (error_message_len > 0) {
+            snprintf((char *)error_message, error_message_len,
+                     "Input 'rownum' cannot be NULL for this function.");
+        }
+        isnull[0] = 1; // Mark result as NULL
         return;
     }
 
-    switch (*rownum) {
-        case 1:
-            strncpy(result_data, "Row One", result_len - 1);
-            break;
-        case 2:
-            strncpy(result_data, "Row Two", result_len - 1);
-            break;
-        case 3:
-            strncpy(result_data, "Row Three", result_len - 1);
-            break;
-        default:
-            strncpy(result_data, "Unknown Row", result_len - 1);
-            break;
-    }
-
-    result_data[result_len - 1] = '\0';
-    isnull[0] = 0;
+    // Example logic to populate result_data
+    snprintf(result_data, result_len, "Row number is %d", *rownum);
+    isnull[0] = 0; // Mark result as not NULL
 }
